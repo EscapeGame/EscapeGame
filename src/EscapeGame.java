761557da2a4f126b733e36ba30b428a33b7887cc
Tatiana Braginets <tbraginets@gmail.com>
2011-11-21 22:13:49 -0800

    test

---
+++ src/EscapeGame.java
@@ -0,0 +1,25 @@
+
+/**
+ * Class that creates a new game
+ * @author Tatiana Braginets
+ * @version 1.1
+ */
+
+public class EscapeGame 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Map map = new Map(MAP_WIDTH, MAP_HEIGHT);
+		Player player = new Player();
+		
+
+	}
+	
+	public static final int MAP_WIDTH = 100;
+	public static final int MAP_HEIGHT = 100;
+
+}
