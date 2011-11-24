293d1307ecc8c0013af87be8a15afeab26e1c627
Carlos J. Castro Vargas <carlos.castro.vargas@gmail.com>
2011-11-24 13:56:04 -0800

    I added the properties to the UML and Player class

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,55 @@
 
 public class Player {
+	
+	public Player(){
+		
+	}
+	
+	public Player(String hp, int strength, int intelligence, int dexterity,
+			int mana) {
+		this.hp = hp;
+		this.strength = strength;
+		this.intelligence = intelligence;
+		this.dexterity = dexterity;
+		this.mana = mana;
+	}
+	
+	private String hp;
+	private int strength;
+	private int intelligence;
+	private int dexterity;
+	private int mana;
+	
+	
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getIntelligence() {
+		return intelligence;
+	}
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+	public int getDexterity() {
+		return dexterity;
+	}
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+	public int getMana() {
+		return mana;
+	}
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
 
 }
