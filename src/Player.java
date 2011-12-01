82abf0898320179750586be1ec5b3f0f7fbd247d
Carlos J. Castro Vargas <carlos.castro.vargas@gmail.com>
2011-11-30 21:14:11 -0800

    added Level class and updated Player class

--- src/Player.java
+++ src/Player.java
@@ -1,54 +1,37 @@
+import java.util.ArrayList;
+
 public class Player {
 	
 	public Player(){
-		
-	}
-	//String hp, should it be int since you'll be adding hp later on when u lvl up 
-	public Player(int hp, int strength, int intelligence, int dexterity,
-			int mana) {
-		this.hp = hp;
-		this.strength = strength;
-		this.intelligence = intelligence;
-		this.dexterity = dexterity;
-		this.mana = mana;
+		gainLevel(levels[0]);
 	}
 	
-	private int hp;
-	private int strength;
-	private int intelligence;
-	private int dexterity;
-	private int mana;
+	private int hp = 0;
+	private int attack = 0;
+	private int defense = 0;
+	private int strength = 0;
+	private int level = 0;
+	private int intelligence = 0;
+	private int dexterity = 0;
+	private int experience = 0;
+	private int mana = 0;
+	private Inventory inventory;
+	private Level[] levels = {new Level(0, 10, 10, 10, 10, 10, 10, 10), 
+			new Level(100, 20, 20, 20, 20, 20, 20, 20)};
 	
 	
-	public int getHp() {
-		return hp;
+	public void gainLevel(Level level){
+		this.hp += level.getHp();
+		this.attack += level.getAttack();
+		this.defense += level.getDefense();
+		this.strength += level.getStrength();
+		this.intelligence += level.getIntelligence();
+		this.dexterity += level.getDexterity();
+		this.mana += level.getMana();
+		this.experience = 0;
+		this.level++;
 	}
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-	public int getStrength() {
-		return strength;
-	}
-	public void setStrength(int strength) {
-		this.strength = strength;
-	}
-	public int getIntelligence() {
-		return intelligence;
-	}
-	public void setIntelligence(int intelligence) {
-		this.intelligence = intelligence;
-	}
-	public int getDexterity() {
-		return dexterity;
-	}
-	public void setDexterity(int dexterity) {
-		this.dexterity = dexterity;
-	}
-	public int getMana() {
-		return mana;
-	}
-	public void setMana(int mana) {
-		this.mana = mana;
-	}
-
+	
+	
+	
 }
