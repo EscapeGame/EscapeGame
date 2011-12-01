e51200bccb5952dd4a402c0ba8cfd0a1a9428af9
Carlos J. Castro Vargas <carlos.castro.vargas@gmail.com>
2011-11-30 21:20:36 -0800

    added Level class and updated Player class

--- src/Player.java
+++ src/Player.java
@@ -31,6 +31,106 @@ public class Player {
 		this.experience = 0;
 		this.level++;
 	}
+
+
+	public int getHp() {
+		return hp;
+	}
+
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+
+	public int getAttack() {
+		return attack;
+	}
+
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+
+	public int getDefense() {
+		return defense;
+	}
+
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+
+	public int getStrength() {
+		return strength;
+	}
+
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+
+	public int getDexterity() {
+		return dexterity;
+	}
+
+
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+
+
+	public int getExperience() {
+		return experience;
+	}
+
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+
+	public int getMana() {
+		return mana;
+	}
+
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
 	
 	
 	
